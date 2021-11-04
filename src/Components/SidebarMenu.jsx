9bd6b2adc69ff802222ebc0e6cb5b934d40cb152
Devon Fruck <devon.fruck@hotmail.com>
2021-11-03 21:59:40 -0400

    Aligned icons and working on sliding animation

--- src/Components/SidebarMenu.jsx
+++ src/Components/SidebarMenu.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Avatar, SvgIcon } from '@material-ui/core'
 import styled from 'styled-components'
-import { ReactComponent as Wave } from '../Assets/wave.svg'
-import PersonIcon from '@mui/icons-material/Person';
+import { Call, BadgeRounded, Menu, Attractions } from '@mui/icons-material'
 
 export const SidebarMenu = () => {
 
@@ -17,24 +15,39 @@ export const SidebarMenu = () => {
       <MenuButton 
         onClick={() => setIsExpanded(!isExpanded)}
       >
-        <PersonIcon />
-        {isExpanded ? 'Menu' : null }
-      </MenuButton >
+        <Menu fontSize='large' style={{ margin:"auto 0" }}/>
+        <span style={{ margin:"auto 0" }}>
+          {isExpanded ? 'Menu' : null }
+        </span>
+      </MenuButton>
       
-      <MenuButton>
-        <PersonIcon />
-        {isExpanded ? 'About Me' : null }
-      </MenuButton >
+      <MenuButton
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        
+        <BadgeRounded fontSize='large' style={{ margin:"auto 0" }}/>
+        <span style={{ margin:"auto 0" }}>
+          {isExpanded ? 'About Me' : null }
+        </span>
+      </MenuButton>
       
-      <MenuButton>
-        <PersonIcon />
-        {isExpanded ? 'Experiences' : null }
-      </MenuButton >
+      <MenuButton
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <Attractions fontSize='large' style={{ margin:"auto 0" }}/>
+        <span style={{ margin:"auto 0" }}>
+          {isExpanded ? 'Experiences' : null }
+        </span>
+      </MenuButton>
 
-      <MenuButton>
-        <PersonIcon />
-        {isExpanded ? 'Contact Me' : null }
-      </MenuButton >
+      <MenuButton
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <Call fontSize='large' style={{ margin:"auto 0" }}/>
+        <span style={{ margin:"auto 0" }}>
+          {isExpanded ? 'Contact Me' : null }
+        </span>
+      </MenuButton>
     </StyledDiv>
   )
 }
@@ -44,12 +57,45 @@ const StyledDiv = styled.div`
   flex-direction: column;
   justify-content: left;
   background-color: red;
-  width: ${({ isExpanded }) => isExpanded ? '10rem':'3rem'};
+  width: ${({ isExpanded }) => isExpanded ? '15rem':'4rem'};
+  transition: width 0.1s linear;
+
+  /* FADE ANIMATIONS */
+  .fade-enter {
+      opacity: 0;
+    }
+
+    .fade-enter-active {
+      opacity: 1;
+      transition: opacity 400ms;
+    }
+
+    .fade-exit {
+      opacity: 1;
+    }
+
+    .fade-exit-active {
+      opacity: 0;
+      transition: opacity 400ms;
+    }
+  
 `
 
 const MenuButton = styled.button `
   display: flex;
-  align-content: flex-start;
-  //height: 5rem;
-  //width: 15rem;
+  height: 5rem;
+  width: 100%;
+  border-radius: 0px;
+
+  &:hover {
+    color: blue;
+  }
+  
+  span {
+    z-index: 1000;
+    font-size: 2rem;
+    padding-left: 1rem;
+
+    
+  }
 `
